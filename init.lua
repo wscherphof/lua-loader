46055c83562796f7a73cdae067e2c45c97f83eb2
Wouter Scherphof <wouter.scherphof@gmail.com>
2013-06-22 05:50:32 +0200

    "version": "0.0.3-1"
    only some code cosmetics

--- init.lua
+++ init.lua
@@ -1,12 +1,11 @@
-local function lpmrequire (main, luarequire)
+local function lua_loader (main, luarequire)
 
   local dirsep = string.match(package.config, "(.)")
   local maininfo = debug.getinfo(main, "S")
   local maindir = string.match(maininfo.source, "@(.*)" .. dirsep)
-  maininfo = nil
+  main, maininfo = nil, nil
 
-  -- return the new require function
-  return function (module)
+  local function newrequire (module)
     local _M -- set by callrequire()
 
     local function try (path)
@@ -35,7 +34,7 @@ local function lpmrequire (main, luarequire)
       end
       -- alas, just couldn't find it here
       return false
-    end
+    end -- function try
 
     -- firstly, try to load it from just here in the source tree
     if try() then return _M end
@@ -44,7 +43,7 @@ local function lpmrequire (main, luarequire)
     local paths, callinfo, calldir, loaddir = {}, debug.getinfo(2, "S")
     if callinfo then calldir = string.match(callinfo.source, "@(.*)" .. dirsep) end
     if calldir then loaddir = string.match(calldir, maindir .. dirsep .. "(.*)")
-                           or string.match(calldir, "%."     .. dirsep .. "(.*)") end
+                           or string.match(calldir, "%."    .. dirsep .. "(.*)") end
     if loaddir then
       loaddir = string.gsub(loaddir, dirsep, ".")
       local path
@@ -59,18 +58,21 @@ local function lpmrequire (main, luarequire)
     for _,path in ipairs(paths) do
       if try(path) then return _M end
     end
-  -- end function (module)
-  end
--- end function lpmrequire
-end
+  end -- function newrequire
+
+  return newrequire
+end -- function lua_loader
 
 -- initialise with some function that is in the project's main lua file,
 -- so that we know what the top of the source tree is,
 -- which is the point that the real require is looking at
 local function init (main)
-  if _LPM then return end
-  _LPM = true
-  require = lpmrequire(main, require)
+  -- prevent messing up the maindir once set
+  if _LUA_LOADER then return end
+  _LUA_LOADER = true
+  -- replace the original require, but pass it in so that we can call it
+  require = lua_loader(main, require)
+  -- new require function all set up now; nothing to return 
 end
 
 return init
